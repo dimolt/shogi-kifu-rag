fba0b49d0dfc8a3efec9d413f87712a3addea8e3
ichi <shots.of.gin@gmail.com>
2026-07-02 18:08:25 +0900

    [fix]マージ漏れ修正を追加

--- src/shogi_kif_rag/kif/parser/__init__.py
+++ src/shogi_kif_rag/kif/parser/__init__.py
@@ -2,8 +2,9 @@
 
 from shogi_kif_rag.kif.parser.kif_parser import (
     KifParser,
+    PositionRecord,
     detect_encoding,
     kif_to_positions,
 )
 
-__all__ = ["KifParser", "detect_encoding", "kif_to_positions"]
+__all__ = ["KifParser", "detect_encoding", "kif_to_positions", "PositionRecord"]
